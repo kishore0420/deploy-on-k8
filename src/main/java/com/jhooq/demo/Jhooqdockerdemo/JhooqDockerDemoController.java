ea252f6e7d065fe79cf95646d86b4540fda40fc1
kishore0420 <51207213+kishore0420@users.noreply.github.com>
2021-11-23 13:45:49 +0530

    addes

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class kishorek8DemoController {
+public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
